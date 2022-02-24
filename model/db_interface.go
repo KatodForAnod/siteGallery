6d1653b7e537481c0700921c191e00b06cf18fcf
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-02-24 14:55:21 +0300

    added db interfaces

--- model/db_interface.go
+++ model/db_interface.go
@@ -1,5 +1,18 @@
 package model
 
 type Database interface {
-	AddImage() error
+	DatabaseImg
+	DatabaseUser
+}
+
+type DatabaseImg interface {
+	AddImage(data ImgMetaData) error
+	RemoveImage(id int64) error
+	GetImage(id int64) (ImgMetaData, error)
+	GetImages(offSet, limit int64) ([]ImgMetaData, error)
+}
+
+type DatabaseUser interface {
+	AddUser(user User) error
+	GetUser(email string) (User, error)
 }

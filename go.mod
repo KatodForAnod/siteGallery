f27ac4d439c7d9a92614a72595ae9416fa3f443f
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-02-28 12:30:33 +0300

    added realization query get images

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module siteGallery
 
 go 1.17
+
+require github.com/lib/pq v1.10.4
--- model/db/db_queries.go
+++ model/db/db_queries.go
@@ -1,6 +1,10 @@
 package db
 
-import "siteGallery/model"
+import (
+	"github.com/lib/pq"
+	"log"
+	"siteGallery/model"
+)
 
 func (p postgreSQl) AddImage(data model.ImgMetaData) error {
 	panic("implement me")
@@ -14,8 +18,33 @@ func (p postgreSQl) GetImage(id int64) (model.ImgMetaData, error) {
 	panic("implement me")
 }
 
+const getImages = `
+	SELECT id, img_data, tags, load_by_user
+	FROM img
+	OFFSET $1
+	LIMIT $2
+`
+
 func (p postgreSQl) GetImages(offSet, limit int64) ([]model.ImgMetaData, error) {
-	panic("implement me")
+	rows, err := p.conn.Query(getImages, offSet, limit)
+	if err != nil {
+		log.Println(err)
+		return []model.ImgMetaData{}, err
+	}
+	defer rows.Close()
+
+	imges := make([]model.ImgMetaData, 0, 10)
+	data := model.ImgMetaData{}
+	for rows.Next() {
+		err := rows.Scan(&data.Id, &data.Data, (*pq.StringArray)(&data.Tags), &data.LoadByUser)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		imges = append(imges, data)
+	}
+
+	return imges, nil
 }
 
 func (p postgreSQl) AddUser(user model.User) error {

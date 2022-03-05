a30c12bf4581a047ac7e889c024f48677a0c7782
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-03-05 14:41:50 +0300

    update Controller - add singleton

--- controller/controller.go
+++ controller/controller.go
@@ -2,11 +2,34 @@ package controller
 
 import (
 	"log"
+	"siteGallery/config"
 	"siteGallery/model"
+	"siteGallery/model/db"
+	"sync"
+)
+
+var (
+	controller Controller
+	sc         sync.Once
 )
 
 type Controller struct {
 	db model.Database
+}
+
+func GetControllerInstance(config config.Config) (Controller, error) {
+	var err error
+	sc.Do(func() {
+		var dbModel model.Database
+		dbModel, err = db.GetPostgresSQlConn(config)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		controller.db = dbModel
+	})
+
+	return controller, err
 }
 
 func (c *Controller) GetImages(offset, limit int64) ([]model.ImgMetaData, error) {

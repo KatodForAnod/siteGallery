df0321cc3456f20fd37ecbf8e2884419c09304d1
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-03-09 19:37:54 +0300

    added jwt tokens

---
+++ cmd/jwt/jwt_token.go
@@ -0,0 +1,38 @@
+package jwt
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
+)
+
+func CreateToken(userID int64) (string, error) {
+	var err error
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userID
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func VerifyToken(tokenStr string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module siteGallery
 go 1.17
 
 require github.com/lib/pq v1.10.4
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect

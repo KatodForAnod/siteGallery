9f8b71d83ed5149a2f868e96153b7e07978dad8e
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-03-09 10:27:01 +0300

    add docker files

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
 	"siteGallery/config"
+	"siteGallery/controller"
 	"siteGallery/view"
 )
 
 func main() {
-	conf := config.Config{}
-	view.StartHttpServer(conf)
+	conf, _ := config.LoadConfig()
+	contrllr, err := controller.GetControllerInstance(conf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	view.StartHttpServer(conf, contrllr)
 }

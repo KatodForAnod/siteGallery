cbaa4a309dbbd34c3176636cd3d28014c63f6755
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-09-23 15:24:59 +0300

    test for db queries

--- internal/db/postgres/db_quries_test.go
+++ internal/db/postgres/db_quries_test.go
@@ -4,6 +4,7 @@ import (
 	"KatodForAnod/siteGallery/internal/models"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -39,7 +40,64 @@ func TestPostgreSQl_AddUser(t *testing.T) {
 }
 
 func TestPostgreSQl_AddImage(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
 
+	testImg := models.ImgMetaData{
+		Id:         0,
+		FileName:   "testImg",
+		Tags:       nil,
+		Data:       "",
+		LoadByUser: "",
+	}
+
+	testPostgres.conn = db
+	mock.ExpectExec("INSERT INTO img").WithArgs(string(testImg.Data), (*pq.StringArray)(&testImg.Tags)).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	if err = testPostgres.AddImage(testImg); err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestPostgreSQl_GetImages(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	testImage := models.ImgMetaData{
+		Id:         0,
+		FileName:   "",
+		Tags:       nil,
+		Data:       "",
+		LoadByUser: "",
+	}
+
+	testPostgres.conn = db
+	rows := sqlmock.NewRows([]string{"id", "img", "tags"}).
+		AddRow(testImage.Id, testImage.Data, (*pq.StringArray)(&testImage.Tags))
+	mock.ExpectQuery("SELECT id, img.img, tags FROM img").
+		WithArgs(0, 10).WillReturnRows(rows)
+
+	images, err := testPostgres.GetImages(0, 10)
+	if err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	assert.Equal(t, []models.ImgMetaData{testImage}, images, "returned array isnt correct")
 }
 
 func TestPostgreSQl_GetUser(t *testing.T) {

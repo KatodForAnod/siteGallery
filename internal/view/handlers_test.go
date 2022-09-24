31acd3dab59a6d99442fc5ebefded0e9aedf8c98
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-09-24 15:01:37 +0300

    added sample of handlers tests

---
+++ internal/view/handlers_test.go
@@ -0,0 +1,32 @@
+package view
+
+import (
+	"testing"
+)
+
+func TestHandlers_GetImagesPage(t *testing.T) {
+}
+
+func TestHandlers_LoadImagePageGet(t *testing.T) {
+}
+
+func TestHandlers_LoadImagePagePost(t *testing.T) {
+}
+
+func TestHandlers_CheckAuth(t *testing.T) {
+}
+
+func TestHandlers_ErrorHandling(t *testing.T) {
+}
+
+func TestHandlers_GetLoginPage(t *testing.T) {
+}
+
+func TestHandlers_Logout(t *testing.T) {
+}
+
+func TestHandlers_Login(t *testing.T) {
+}
+
+func TestHandlers_GetRegistrationPage(t *testing.T) {
+}

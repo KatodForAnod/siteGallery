b2de9eff6715b9dce16f5214cf96bf65753491b5
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-10-02 01:28:41 +0300

    fix cast var

--- internal/view/jwt_token.go
+++ internal/view/jwt_token.go
@@ -12,7 +12,7 @@ func CreateToken(userId int64) (string, error) {
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["user_id"] = userId
-	atClaims["exp"] = time.Now().Add(time.Minute * 1).Unix()
+	atClaims["exp"] = time.Now().Add(time.Minute * 10).Unix()
 
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
@@ -39,10 +39,10 @@ func VerifyToken(tokenStr string) (*jwt.Token, error) { // TODO func name verify
 
 func GetUserIdFromToken(token *jwt.Token) (int64, error) {
 	claims := token.Claims.(jwt.MapClaims)
-	userId, ok := claims["user_id"].(int64)
+	userId, ok := claims["user_id"].(float64)
 	if !ok {
 		return 0, fmt.Errorf("wrong claims data")
 	}
 
-	return userId, nil
+	return int64(userId), nil
 }
